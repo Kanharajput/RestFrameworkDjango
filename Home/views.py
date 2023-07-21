c1b767f8395499cf225a4161c0bd51a8bada1a14
Kanha Tomar <kanharajput91@gmail.com>
2023-07-21 19:14:06 +0530

    api_view decorator

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# now this will only accept the get request
+# using @api_view decorator now it won't remain the same view method
+# GET describe here that it only accept the GET request from GET,POST,PUT,PATCH and DELETE
+@api_view(['GET'])
 def home(request):
-    return HttpResponse("Home app created")
+    return Response({'first-name: kanha', "second-name:tomar"})
+
