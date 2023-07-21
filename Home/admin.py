00eca286dd26eb361c4080c137297102bb9bfac1
Kanha Tomar <kanharajput91@gmail.com>
2023-07-21 19:42:15 +0530

    model created and migrated

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Students
 
 # Register your models here.
+# register here to access the model from admin panel
+admin.site.register(Students)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+'''
+we are using the sqlite3 database which is already connected with this projects
+check settings
+'''
 # Create your models here.
+class Students(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Students"
+
+    name = models.CharField(max_length=100)
+    section = models.CharField(max_length=20)
+    phone_no = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return self.name
