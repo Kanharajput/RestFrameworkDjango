2f3b767df0844bb906942b2f4f64f7b7080d6652
Kanha Tomar <kanharajput91@gmail.com>
2023-07-25 21:03:03 +0530

    add books from category section inside admin panel

--- AnotherApp/admin.py
+++ AnotherApp/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Category, Book
+from .models import *
 
-# Register your models here.
-admin.site.register(Category)
 admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
